3c2832e80ceeaf7becceceb81c9fd4cb3962de96
eliezerwohl <eliezerwohl@gmail.com>
2015-11-12 21:12:11 -0500

    added smaller than, string or not

---
+++ js.js
@@ -0,0 +1,27 @@
+prompt('herro');
+
+
+var firstValue = prompt("Pick a number.");
+
+
+
+firstValue = parseInt(firstValue);
+if(isNaN(firstValue)) {
+  alert("string!");
+} else {
+  var secondValue = prompt("not a string");
+  }
+
+
+
+var bigOrSmall1 = prompt("pick a number fool!")
+var bigOrSmall2 = prompt("pick another number, fool")
+
+if (bigOrSmall1 < bigOrSmall2) {
+  alert(bigOrSmall1 + " " + "is smaller than" + " " + bigOrSmall2)
+
+  }
+
+  else {
+    alert(bigOrSmall2 + " " +"is smaller than" + " " + bigOrSmall1)
+  }
